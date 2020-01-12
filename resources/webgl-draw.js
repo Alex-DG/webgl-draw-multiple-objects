4e860dc25be39c80d807c6c47e6198889db34de6
Alex-DG <alexandre.diguida@gmail.com>
2020-01-12 11:01:22 +0000

    ♻️ Create array of objects to draw and loop on them

--- resources/webgl-draw.js
+++ resources/webgl-draw.js
@@ -34,156 +34,188 @@ void main() {
 `;
 
 function main() {
+  // Get A WebGL context
+  /** @type {HTMLCanvasElement} */
+  var canvas = document.getElementById('canvas');
+  var gl = canvas.getContext('webgl2');
 
-    // Get A WebGL context
-    /** @type {HTMLCanvasElement} */
-    var canvas = document.getElementById('canvas');
-    var gl = canvas.getContext('webgl2');
+  if (!gl) {
+    alert('Your environement is not compatible with WebGL 2.0');
+    return;
+  }
 
-    if (!gl) {
-        alert('Your environement is not compatible with WebGL 2.0');
-        return;
-    }
+  // Tell the twgl to match position with a_position, n
+  // normal with a_normal etc..
+  twgl.setAttributePrefix('a_');
 
-    // Tell the twgl to match position with a_position, n
-    // normal with a_normal etc..
-    twgl.setAttributePrefix("a_");
+  var sphereBufferInfo = flattenedPrimitives.createSphereBufferInfo(
+    gl,
+    10,
+    12,
+    6
+  );
+  var cubeBufferInfo = flattenedPrimitives.createCubeBufferInfo(gl, 20);
+  var coneBufferInfo = flattenedPrimitives.createTruncatedConeBufferInfo(
+    gl,
+    10,
+    0,
+    20,
+    12,
+    1,
+    true,
+    false
+  );
 
-    var sphereBufferInfo = flattenedPrimitives.createSphereBufferInfo(gl, 10, 12, 6);
-    var cubeBufferInfo   = flattenedPrimitives.createCubeBufferInfo(gl, 20);
-    var coneBufferInfo   = flattenedPrimitives.createTruncatedConeBufferInfo(gl, 10, 0, 20, 12, 1, true, false);
+  // setup GLSL program
+  var programInfo = twgl.createProgramInfo(gl, [vs, fs]);
 
-    // setup GLSL program
-    var programInfo = twgl.createProgramInfo(gl, [vs, fs]);
+  var sphereVAO = twgl.createVAOFromBufferInfo(
+    gl,
+    programInfo,
+    sphereBufferInfo
+  );
+  var cubeVAO = twgl.createVAOFromBufferInfo(gl, programInfo, cubeBufferInfo);
+  var coneVAO = twgl.createVAOFromBufferInfo(gl, programInfo, coneBufferInfo);
 
-    var sphereVAO = twgl.createVAOFromBufferInfo(gl, programInfo, sphereBufferInfo);
-    var cubeVAO   = twgl.createVAOFromBufferInfo(gl, programInfo, cubeBufferInfo);
-    var coneVAO   = twgl.createVAOFromBufferInfo(gl, programInfo, coneBufferInfo);
+  function degToRad(d) {
+    return (d * Math.PI) / 180;
+  }
 
-    function degToRad(d) {
-        return d * Math.PI / 180;
-    }
+  var fieldOfViewRadians = degToRad(60);
 
-    var fieldOfViewRadians = degToRad(60);
-
-
-    // Our uniforms for each thing we want to draw 
-    var sphereUniforms = {
+  // Our uniforms for each thing we want to draw
+  var sphereUniforms = {
     u_colorMult: [0.5, 1, 0.5, 1],
-    u_matrix: m4.identity(),
-    };
-    var cubeUniforms = {
+    u_matrix: m4.identity()
+  };
+  var cubeUniforms = {
     u_colorMult: [1, 0.5, 0.5, 1],
-    u_matrix: m4.identity(),
-    };
-    var coneUniforms = {
+    u_matrix: m4.identity()
+  };
+  var coneUniforms = {
     u_colorMult: [0.5, 0.5, 1, 1],
-    u_matrix: m4.identity(),
-    };
+    u_matrix: m4.identity()
+  };
 
-    // The translation for each object.
-    var sphereTranslation = [  0, 0, 0];
-    var cubeTranslation   = [-40, 0, 0];
-    var coneTranslation   = [ 40, 0, 0];
-    
-    function computeMatrix(viewProjectionMatrix, translation, xRotation, yRotation) {
-        var matrix = m4.translate(viewProjectionMatrix,
-            translation[0],
-            translation[1],
-            translation[2]);
-        matrix = m4.xRotate(matrix, xRotation);
-        return m4.yRotate(matrix, yRotation);
+  // The translation for each object.
+  var sphereTranslation = [0, 0, 0];
+  var cubeTranslation = [-40, 0, 0];
+  var coneTranslation = [40, 0, 0];
+
+  var objectsToDraw = [
+    {
+      programInfo: programInfo,
+      bufferInfo: sphereBufferInfo,
+      vertexArray: sphereVAO,
+      uniforms: sphereUniforms
+    },
+    {
+      programInfo: programInfo,
+      bufferInfo: cubeBufferInfo,
+      vertexArray: cubeVAO,
+      uniforms: cubeUniforms
+    },
+    {
+      programInfo: programInfo,
+      bufferInfo: coneBufferInfo,
+      vertexArray: coneVAO,
+      uniforms: coneUniforms
     }
+  ];
+
+  function computeMatrix(
+    viewProjectionMatrix,
+    translation,
+    xRotation,
+    yRotation
+  ) {
+    var matrix = m4.translate(
+      viewProjectionMatrix,
+      translation[0],
+      translation[1],
+      translation[2]
+    );
+    matrix = m4.xRotate(matrix, xRotation);
+    return m4.yRotate(matrix, yRotation);
+  }
+
+  requestAnimationFrame(drawScene);
+
+  // Draw the scene.
+  function drawScene(time) {
+    time = time * 0.0005;
+
+    twgl.resizeCanvasToDisplaySize(gl.canvas);
+
+    // Tell WebGL how to convert from clip space to pixels
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    gl.enable(gl.CULL_FACE);
+    gl.enable(gl.DEPTH_TEST);
+
+    // Compute the projection matrix
+    var aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    var projectionMatrix = m4.perspective(fieldOfViewRadians, aspect, 1, 2000);
+
+    // Compute the camera's matrix using look at.
+    var cameraPosition = [0, 0, 100];
+    var target = [0, 0, 0];
+    var up = [0, 1, 0];
+    var cameraMatrix = m4.lookAt(cameraPosition, target, up);
+
+    // Make a view matrix from the camera matrix.
+    var viewMatrix = m4.inverse(cameraMatrix);
+
+    var viewProjectionMatrix = m4.multiply(projectionMatrix, viewMatrix);
+
+    var sphereXRotation = time;
+    var sphereYRotation = time;
+    var cubeXRotation = -time;
+    var cubeYRotation = time;
+    var coneXRotation = time;
+    var coneYRotation = -time;
+
+    // ------ Compute Matrices --------
+
+    sphereUniforms.u_matrix = computeMatrix(
+      viewProjectionMatrix,
+      sphereTranslation,
+      sphereXRotation,
+      sphereYRotation
+    );
+
+    cubeUniforms.u_matrix = computeMatrix(
+      viewProjectionMatrix,
+      cubeTranslation,
+      cubeXRotation,
+      cubeYRotation
+    );
+
+    coneUniforms.u_matrix = computeMatrix(
+      viewProjectionMatrix,
+      coneTranslation,
+      coneXRotation,
+      coneYRotation
+    );
+
+    // ------ Draw the objects --------
+
+    objectsToDraw.forEach(function(object) {
+      var programInfo = object.programInfo;
+
+      gl.useProgram(programInfo.program);
+
+      // Setup all the needed attributes.
+      gl.bindVertexArray(object.vertexArray);
+
+      // Set the uniforms we just computed
+      twgl.setUniforms(programInfo, object.uniforms);
+
+      twgl.drawBufferInfo(gl, object.bufferInfo);
+    });
 
     requestAnimationFrame(drawScene);
-
-    // Draw the scene.
-    function drawScene(time) {
-        time = time * 0.0005;
-
-        twgl.resizeCanvasToDisplaySize(gl.canvas);
-
-        // Tell WebGL how to convert from clip space to pixels
-        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-        gl.enable(gl.CULL_FACE);
-        gl.enable(gl.DEPTH_TEST);
-
-        // Compute the projection matrix
-        var aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-        var projectionMatrix =
-            m4.perspective(fieldOfViewRadians, aspect, 1, 2000);
-
-        // Compute the camera's matrix using look at.
-        var cameraPosition = [0, 0, 100];
-        var target = [0, 0, 0];
-        var up = [0, 1, 0];
-        var cameraMatrix = m4.lookAt(cameraPosition, target, up);
-
-        // Make a view matrix from the camera matrix.
-        var viewMatrix = m4.inverse(cameraMatrix);
-
-        var viewProjectionMatrix = m4.multiply(projectionMatrix, viewMatrix);
-
-        var sphereXRotation =  time;
-        var sphereYRotation =  time;
-        var cubeXRotation   = -time;
-        var cubeYRotation   =  time;
-        var coneXRotation   =  time;
-        var coneYRotation   = -time;
-
-        gl.useProgram(programInfo.program);
-
-        // ------ Draw the sphere --------
-
-        // Setup all the needed attributes.
-        gl.bindVertexArray(sphereVAO);
-
-        sphereUniforms.u_matrix = computeMatrix(
-            viewProjectionMatrix,
-            sphereTranslation,
-            sphereXRotation,
-            sphereYRotation);
-
-        // Set the uniforms we just computed
-        twgl.setUniforms(programInfo, sphereUniforms);
-
-        twgl.drawBufferInfo(gl, sphereBufferInfo);
-
-        // ------ Draw the cube --------
-
-        // Setup all the needed attributes.
-        gl.bindVertexArray(cubeVAO);
-
-        cubeUniforms.u_matrix = computeMatrix(
-            viewProjectionMatrix,
-            cubeTranslation,
-            cubeXRotation,
-            cubeYRotation);
-
-        // Set the uniforms we just computed
-        twgl.setUniforms(programInfo, cubeUniforms);
-
-        twgl.drawBufferInfo(gl, cubeBufferInfo);
-
-        // ------ Draw the cone --------
-
-        // Setup all the needed attributes.
-        gl.bindVertexArray(coneVAO);
-
-        coneUniforms.u_matrix = computeMatrix(
-            viewProjectionMatrix,
-            coneTranslation,
-            coneXRotation,
-            coneYRotation);
-
-        // Set the uniforms we just computed
-        twgl.setUniforms(programInfo, coneUniforms);
-
-        twgl.drawBufferInfo(gl, coneBufferInfo);
-
-        requestAnimationFrame(drawScene);
-    }
+  }
 }
 
 main();
